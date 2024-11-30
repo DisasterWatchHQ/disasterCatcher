7d952725b400816b2b53e713464099f7b74fe848
5yndr0m <sdilanjana21@gmail.com>
2024-11-30 22:31:50 +0530

    fix : fixed a typo in package import

--- models/users.js
+++ models/users.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const locationSchema = new Schema({
   latitude: { type: Number, required: true },
