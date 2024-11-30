2293dc6dc993f1cb91e7c393ab85944505df6113
5yndr0m <sdilanjana21@gmail.com>
2024-11-30 21:13:54 +0530

    feat: improved user model
    added admin tag to check user profile states

--- models/users.js
+++ models/users.js
@@ -10,7 +10,11 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  type: { type: String, required: true, enum: ["normal", "verified", "anonymous"] },
+  type: { 
+    type: String, 
+    required: true, 
+    enum: ["registered", "verified", "anonymous", "admin"],
+    default: "anonymous" },
   location: { type: locationSchema, required: false },
   associated_department: { 
     type: String, 
