254381e4177c34b2ca3e2368708e9eebbf0cd11a
5yndr0m <sdilanjana21@gmail.com>
2024-12-09 20:33:26 +0530

    fix: fixed some incorrect imports

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import User from '../models/user.js';
+import User from '../models/users.js';
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,23 @@
 import express from 'express';
 import { 
   createUser, 
-  getUsers, 
+  getAllUsers, 
   getUserById, 
   updateUser, 
   deleteUser, 
-  loginUser 
+  authenticateUser,
+  changePassword 
 } from '../controllers/userController.js';
 import { protectRoute, verifyUserType } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/login', loginUser);
+router.post('/login', authenticateUser);
 
 router.use(protectRoute);
 
 router.post('/', verifyUserType('admin'), createUser);
-router.get('/', verifyUserType('admin'), getUsers);
+router.get('/', verifyUserType('admin'), getAllUsers);
 router.get('/:id', getUserById);
 router.put('/:id', verifyUserType('admin'), updateUser);
 router.delete('/:id', verifyUserType('admin'), deleteUser);
