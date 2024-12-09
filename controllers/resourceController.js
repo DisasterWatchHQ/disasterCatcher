2a4f398f028766b39537e21fb86c5facd9394299
5yndr0m <sdilanjana21@gmail.com>
2024-12-09 21:10:11 +0530

    feat: adding better pagination for querying

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -18,7 +18,52 @@ export const createResource = async (req, res) => {
 
 export const getResources = async (req, res) => {
   try {
-    const resources = await Resource.find();
+    const {
+      type,
+      availability_status,
+      name,
+      city,
+      district,
+      province,
+      limit = 10,
+      page = 1
+    } = req.query;
+
+    // Build query object
+    const query = {};
+
+    // Add filters if they exist
+    if (type) query.type = type;
+    if (availability_status) query.availability_status = availability_status;
+    if (name) query.name = { $regex: name, $options: 'i' }; // Case-insensitive search
+    
+    // Location filters
+    if (city || district || province) {
+      query['location.address'] = {};
+      if (city) query['location.address.city'] = { $regex: city, $options: 'i' };
+      if (district) query['location.address.district'] = { $regex: district, $options: 'i' };
+      if (province) query['location.address.province'] = { $regex: province, $options: 'i' };
+    }
+
+    // Pagination
+    const skip = (page - 1) * limit;
+
+    const resources = await Resource.find(query)
+      .limit(parseInt(limit))
+      .skip(skip);
+
+    const total = await Resource.countDocuments(query);
+
+    res.status(200).json({
+      resources,
+      currentPage: page,
+      totalPages: Math.ceil(total / limit),
+      totalResults: total
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
     res.status(200).json(resources);
   } catch (error) {
     res.status(500).json({ error: error.message });
