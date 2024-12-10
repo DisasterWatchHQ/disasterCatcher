d1a42081e1a96e4a21aa3e7957069124c5294182
sharada0417 <sharada.kanchana417@gmail.com>
2024-12-10 23:56:45 +0530

    Add resourceRoutes.js

--- routes/resourceRoutes.js
+++ routes/resourceRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { 
   createResource, 
   getResources, 
+  getNearbyResources,
   getResourceById, 
   updateResource, 
   deleteResource 
@@ -12,10 +13,14 @@ const router = express.Router();
 
 router.use(protectRoute);
 
-router.post('/', createResource);
+router.post('/', verifyUserType(['admin', 'manager']), createResource);
+
 router.get('/', getResources);
+router.get('/nearby', getNearbyResources);
 router.get('/:id', getResourceById);
-router.put('/:id', updateResource);
-router.delete('/:id', deleteResource);
+
+router.put('/:id', verifyUserType(['admin', 'manager']), updateResource);
+
+router.delete('/:id', verifyUserType(['admin', 'manager']), deleteResource);
 
 export default router;
