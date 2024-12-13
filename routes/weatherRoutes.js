4ab126fbdb260de94f4e0fed9ff3f64c7c3cc480
5yndr0m <sdilanjana21@gmail.com>
2024-12-13 23:21:29 +0530

    feat: adding new routes and removing old

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,21 +1,17 @@
 import express from 'express';
 import { 
-  createWeatherData, 
-  getWeatherData, 
-  getWeatherDataById, 
-  updateWeatherData, 
-  deleteWeatherData
-} from '../controllers/weatherDataController.js';
+  getCurrentLocationWeather,
+  getSavedLocationWeather,
+  saveLocation
+} from '../controllers/weatherController.js';
 import { protectRoute, verifyUserType } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.use(protectRoute, verifyUserType(["admin"]));
 
-router.post('/', createWeatherData);
-router.get('/', getWeatherData);
-router.get('/:id', getWeatherDataById);
-router.put('/:id', updateWeatherData);
-router.delete('/:id', deleteWeatherData);
+router.get('/current', getCurrentLocationWeather);
+router.get('/saved/:locationId', getSavedLocationWeather);
+router.post('/locations', saveLocation);
 
 export default router;
