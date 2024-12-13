5607b23c66cd7d4ddb91812d6bf022f87701e3e3
5yndr0m <sdilanjana21@gmail.com>
2024-12-13 06:47:58 +0530

    feat: adding the finished routes to index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import adminLogsRoutes from './adminLogsRoutes.js';
 import userRoutes from './userRoutes.js';
+import resourceRoutes from './resourceRoutes.js';
 
 const router = express.Router();
 
-router.use('/adminLogs', adminLogsRoutes);
+router.use('/adminlogs', adminLogsRoutes);
 router.use('/user', userRoutes);
+router.use('/resource', resourceRoutes);
 
 export default router;
