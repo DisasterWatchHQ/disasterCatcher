1fda1145b3f026d4d075363c9a96be8f587ba1e2
5yndr0m <sdilanjana21@gmail.com>
2024-12-13 06:43:05 +0530

    feat: removed update and elete functions for adminlogs
    both functions are not needed. admin logs will be kept saved

--- controllers/adminLogsController.js
+++ controllers/adminLogsController.js
@@ -70,23 +70,4 @@ export const getAdminLogById = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-
-export const updateAdminLog = async (req, res) => {
-  try {
-    const updatedLog = await AdminLog.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    if (!updatedLog) return res.status(404).json({ message: 'Admin Log not found' });
-    res.status(200).json(updatedLog);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-export const deleteAdminLog = async (req, res) => {
-  try {
-    const deletedLog = await AdminLog.findByIdAndDelete(req.params.id);
-    if (!deletedLog) return res.status(404).json({ message: 'Admin Log not found' });
-    res.status(200).json({ message: 'Admin Log deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 };
