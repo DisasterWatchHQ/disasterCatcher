be0fe87c3827c5b4e98336479046b8fad3f82423
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-12-22 11:45:57 +0530

    fixed:index.js

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ import incidentReportsRoutes from './incidentReportsRoutes.js';
 import feedbackRoutes from './feedbackRoutes.js';
 import weatherRoutes from './weatherRoutes.js';
 import alertsRoute from './alertsRoutes.js';
+import notificationRoutes from './notificationRoutes.js';
+import subscriptionRoutes from './subscriptionRoutes.js';
+import locationRoutes from './locationRoutes.js';
 
 const router = express.Router();
 
@@ -18,5 +21,8 @@ router.use('/incidentReport', incidentReportsRoutes);
 router.use('/feedback', feedbackRoutes);
 router.use('/weather', weatherRoutes);
 router.use('/alerts',alertsRoute);
+router.use('/notifications', notificationRoutes);
+router.use('/subscriptions', subscriptionRoutes);
+router.use('/location', locationRoutes);
 
 export default router;
