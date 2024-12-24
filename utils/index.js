00d85396a0fbea232b92dcb2f92e2cb2555502c6
5yndr0m <sdilanjana21@gmail.com>
2024-12-24 23:35:05 +0530

    feat: add a jwt token creation

--- utils/index.js
+++ utils/index.js
@@ -53,3 +53,18 @@ export const connectMongoose = async () => {
 //     throw error;
 //   }
 // };
+//
+export const createJWT = (res, userId) => {
+  //creates JWT token
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  // Change sameSite from strict to none when you deploy your app
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "dev",
+    sameSite: "strict", //prevent CSRF attack
+    maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
+  });
+};
