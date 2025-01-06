aa360bdfa6cbaf505319dee657de6c5f88eeeb2d
5yndr0m <sdilanjana21@gmail.com>
2025-01-06 14:14:16 +0530

    chor: code cleanup

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ import feedbackRoutes from "./feedbackRoutes.js";
 import weatherRoutes from "./weatherRoutes.js";
 import alertsRoute from "./alertsRoutes.js";
 import notificationRoutes from "./notificationRoutes.js";
-import subscriptionRoutes from "./subscriptionRoutes.js";
 import locationRoutes from "./locationRoutes.js";
 
 const router = express.Router();
@@ -22,7 +21,6 @@ router.use("/feedback", feedbackRoutes);
 router.use("/weather", weatherRoutes);
 router.use("/alerts", alertsRoute);
 router.use("/notifications", notificationRoutes);
-router.use("/subscriptions", subscriptionRoutes);
 router.use("/location", locationRoutes);
 
 export default router;
