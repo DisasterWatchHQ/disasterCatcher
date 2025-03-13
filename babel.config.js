47a7c3cfd2d766aff35fffc38611ee5eb9c187e1
5yndr0m <sdilanjana21@gmail.com>
2025-03-13 22:31:52 +0530

    build: add eslint

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,11 @@
 export default {
   presets: [
-    ['@babel/preset-env', {
-      targets: { node: 'current' },
-      modules: 'auto'
-    }],
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: "current" },
+        modules: "auto",
+      },
+    ],
   ],
-}; 
+};
